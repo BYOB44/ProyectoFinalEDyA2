87abc8da7b168443ec08a9a8bb04f3dbfd779041
Djguty1 <santiagogutierrezlozano3@gmail.com>
2026-05-17 17:41:17 -0500

    Fix final allowed origin patterns

--- facturacion-backend/src/main/java/com/empresa/facturacion/config/SecurityConfig.java
+++ facturacion-backend/src/main/java/com/empresa/facturacion/config/SecurityConfig.java
@@ -71,16 +71,21 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
+
+        configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
                 "https://facturacionedya2.netlify.app"));
+
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 }
